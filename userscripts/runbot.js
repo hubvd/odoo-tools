4bf632d10335f9897236b94bac6b12119bb49edf
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-03-24 18:35:22 +0100

    blah

---
+++ userscripts/runbot.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        runbot
+// @match       https://runbot.odoo.com/
+// @version     1.0
+// @author      hubvd
+// ==/UserScript==
+
+$('div.row.bundle_row div.d-none').removeClass('d-none')
