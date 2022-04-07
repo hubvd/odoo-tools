9f6bc8c7dff7c646d3b2e30048436b2eb8d64978
Hubert Van De Walle <hubv@protonmail.com>
2022-04-07 13:33:28 +0200

    abcd

--- debug-addon/debug/controllers/debug.py
+++ debug-addon/debug/controllers/debug.py
@@ -11,6 +11,8 @@ class DebugController(Controller):
             return Response(response='ok')
         except ImportError:
             return Response(status=500, response='pydevd_pycharm not installed\n run pip install pydevd-pycharm~=213.7172.26')
+        except Exception:
+            return Response(status=500, response='An error occurred, maybe the dev server isn\'t started')
 
     @route("/debug/detach", auth="none")
     def detach(self):
@@ -20,3 +22,5 @@ class DebugController(Controller):
             return Response(response='ok')
         except ImportError:
             return Response(status=500, response='pydevd_pycharm not installed\n run pip install pydevd-pycharm~=213.7172.26')
+        except Exception:
+            return Response(status=500, response='An error occurred, maybe the dev server isn\'t started')
