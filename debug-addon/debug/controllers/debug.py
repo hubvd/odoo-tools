c74b7de738f8e89a192140f9b8947fea96307bdb
Hubert Van De Walle <hubv@protonmail.com>
2022-04-07 13:10:57 +0200

    Add attach script & import debug addon

--- debug-addon/debug/controllers/debug.py
+++ debug-addon/debug/controllers/debug.py
@@ -1,18 +1,22 @@
 from odoo.http import Controller, route, Response
 
-
 class DebugController(Controller):
-    is_enabled = False
 
-    @route("/debug", auth="none")
-    def debug(self):
-        if not self.is_enabled:
-            try:
-                import pydevd_pycharm
-                pydevd_pycharm.settrace('localhost', port=10000, stdoutToServer=True, stderrToServer=True, suspend=False)
-                self.is_enabled = True
-                return Response(response='ok')
-            except ImportError:
-                return Response(status=500, response='pydevd_pycharm not installed\n run pip install pydevd-pycharm~=213.7172.26')
-        else:
-            return Response(response='already enabled')
+    @route("/debug/attach", auth="none")
+    def attach(self, port='10000'):
+        try:
+            import pydevd
+            pydevd.stoptrace()
+            pydevd.settrace('localhost', port=int(port), stdoutToServer=True, stderrToServer=True, suspend=False)
+            return Response(response='ok')
+        except ImportError:
+            return Response(status=500, response='pydevd_pycharm not installed\n run pip install pydevd-pycharm~=213.7172.26')
+
+    @route("/debug/detach", auth="none")
+    def detach(self):
+        try:
+            import pydevd
+            pydevd.stoptrace()
+            return Response(response='ok')
+        except ImportError:
+            return Response(status=500, response='pydevd_pycharm not installed\n run pip install pydevd-pycharm~=213.7172.26')
