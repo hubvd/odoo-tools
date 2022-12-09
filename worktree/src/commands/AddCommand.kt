261e3cd5e2642d5ad81f9e3f0d09f3bf28a823c9
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-12-09 12:18:01 +0100

    Add custom directory support for new worktrees

--- worktree/src/commands/AddCommand.kt
+++ worktree/src/commands/AddCommand.kt
@@ -2,9 +2,11 @@ package com.github.hubvd.odootools.worktree.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.CliktError
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.path
 import com.github.ajalt.mordant.rendering.TextColors
 import com.github.ajalt.mordant.rendering.TextStyles
 import com.github.ajalt.mordant.terminal.Terminal
@@ -15,6 +17,7 @@ import com.github.hubvd.odootools.worktree.OdooStubs
 import com.github.hubvd.odootools.worktree.Virtualenvs
 import com.github.hubvd.odootools.worktree.createGitWorktrees
 import com.github.hubvd.odootools.worktree.processSequence
+import kotlin.io.path.Path
 import kotlin.io.path.createDirectory
 import kotlin.io.path.div
 import kotlin.io.path.exists
@@ -29,12 +32,13 @@ class AddCommand(
 ) : CliktCommand() {
     private val name by option().required()
     private val base by option().required()
+    private val path by option().path(canBeFile = false).default(config.root)
 
     private val community by option().flag()
 
     override fun run() {
         if (workspaces.list().find { it.name == name } != null) throw CliktError("$name already exists")
-        val path = config.root / name
+        val path = path / name
         if (path.exists()) throw CliktError("$path already exists")
         path.createDirectory()
         processSequence(terminal) {
