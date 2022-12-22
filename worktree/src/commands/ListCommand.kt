a91ad9e02a54b4a75f536bcc07c4c0ad8ca8e9dc
Hubert Van de Walle (huvw) <huvw@odoo.com>
2022-12-22 11:13:37 +0100

    Print a table for worktrees when the tty is interactive

--- worktree/src/commands/ListCommand.kt
+++ worktree/src/commands/ListCommand.kt
@@ -2,15 +2,43 @@ package com.github.hubvd.odootools.worktree.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.arguments.default
+import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.types.enum
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
+import com.github.ajalt.mordant.table.Borders
+import com.github.ajalt.mordant.table.table
 import com.github.ajalt.mordant.terminal.Terminal
 import com.github.hubvd.odootools.workspace.WorkspaceFormat
 import com.github.hubvd.odootools.workspace.Workspaces
 import com.github.hubvd.odootools.workspace.format
 
 class ListCommand(private val workspaces: Workspaces, private val terminal: Terminal) : CliktCommand() {
-    private val format by argument().enum<WorkspaceFormat>().default(WorkspaceFormat.Json)
-    override fun run() = workspaces.list()
-        .forEach { workspace -> terminal.println(workspace.format(format)) }
+    private val format by argument().enum<WorkspaceFormat>().optional()
+    override fun run() {
+        val workspaces = workspaces.list()
+        if (terminal.info.outputInteractive && format == null) {
+            terminal.println(table {
+                tableBorders = Borders.ALL
+                header {
+                    style(TextColors.magenta, bold = true)
+                    row("name", "path", "version", "base")
+                }
+                body {
+                    rowStyles(TextColors.blue, TextColors.green)
+                    cellBorders = Borders.LEFT_RIGHT
+                    workspaces.forEach { w ->
+                        row {
+                            cell(w.name)
+                            cell(TextStyles.hyperlink("file://${w.path}")(w.path.toString()))
+                            cell(w.version)
+                            cell(w.base)
+                        }
+                    }
+                }
+            })
+        } else workspaces.forEach {
+            terminal.println(it.format(format ?: WorkspaceFormat.Json))
+        }
+    }
 }
