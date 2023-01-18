8493751feb90cd4713f0f611675a66021b4802bc
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-01-18 22:48:23 +0100

    Embed custom launcher inside binary

--- odoo/build.gradle.kts
+++ odoo/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.zip.Adler32
+
 plugins {
     id("cli-application")
 }
@@ -15,3 +17,31 @@ dependencies {
     implementation(libs.xmlbuilder)
     implementation(libs.kodein.di)
 }
+
+sourceSets["main"].resources.srcDir(buildDir.resolve("generated-resources"))
+
+tasks.register("generateLauncherChecksums") {
+    val path = sourceSets["main"].resources.srcDirs.first().resolve("launcher")
+    dependsOn(sourceSets["main"].resources)
+    doLast {
+        val adler32 = Adler32()
+        val checkSums = StringBuilder()
+        path.walkTopDown().filter { it.isFile && it.name.endsWith(".py") }.forEach { file ->
+            val bytes = file.readBytes()
+            adler32.reset()
+            adler32.update(bytes)
+
+            checkSums.append(adler32.value)
+            checkSums.append(':')
+            checkSums.append(file.relativeTo(path))
+            checkSums.appendLine()
+        }
+
+        buildDir.resolve("generated-resources/launcher")
+            .apply { mkdirs() }
+            .resolve("checksums")
+            .writeText(checkSums.toString())
+    }
+}
+
+tasks.getByName("processResources").dependsOn("generateLauncherChecksums")
--- odoo/src/actions/LaunchAction.kt
+++ odoo/src/actions/LaunchAction.kt
@@ -3,7 +3,15 @@ package com.github.hubvd.odootools.odoo.actions
 import com.github.hubvd.odootools.odoo.RunConfiguration
 import com.github.pgreze.process.process
 import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.io.FileNotFoundException
+import java.nio.file.Path
+import java.util.zip.Adler32
 import kotlin.concurrent.thread
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.div
+import kotlin.io.path.outputStream
 import kotlin.system.exitProcess
 
 interface Action {
@@ -11,16 +19,23 @@ interface Action {
 }
 
 class LaunchAction : Action {
+
     override fun run(configuration: RunConfiguration) {
-        val cmd = if ("no-patch" in configuration.context.flags || configuration.context.workspace.version < 14) {
-            listOf("venv/bin/python", "odoo/odoo-bin")
+        val useCustomLauncher = "no-patch" !in configuration.context.flags &&
+            configuration.context.workspace.version < 14
+
+        val main = if (useCustomLauncher) {
+            "odoo/odoo-bin"
         } else {
-            listOf("venv/bin/python", "/home/hubert/odoo-tools/patches/main.py") // FIXME: config ?
+            val launcherDir = unpackPatchedLauncher()
+            (launcherDir / "main.py").toString()
         }
+
+        val cmd = listOf("venv/bin/python", main, *configuration.args.toTypedArray())
 
         val process =
             ProcessBuilder()
-                .command(cmd + configuration.args)
+                .command(cmd)
                 .inheritIO()
                 .apply { environment().putAll(configuration.env) }
                 .directory(configuration.context.workspace.path.toFile())
@@ -47,5 +62,55 @@ class LaunchAction : Action {
         }
 
         exitProcess(code)
+    }
+
+    private fun unpackPatchedLauncher(): Path {
+        val dataDir = Path(System.getenv("XDG_DATA_DIRS") ?: (System.getProperty("user.home") + "/.local/share"))
+        val launcherDir = (dataDir / "odoo-tools/launcher").apply { createDirectories() }
+
+        val expectedChecksums = parseChecksums()
+
+        val actualChecksums: HashMap<Long, String> =
+            launcherDir.toFile().walkTopDown().filter { it.isFile && it.name.endsWith(".py") }
+                .associateTo(HashMap()) { file ->
+                    file.checksum() to file.relativeTo(launcherDir.toFile()).toString()
+                }
+
+        if (expectedChecksums != actualChecksums) {
+            launcherDir.toFile().deleteRecursively()
+            launcherDir.createDirectories()
+
+            val files = expectedChecksums.values
+            files.map { (launcherDir / it).parent }.toHashSet().forEach { it.createDirectories() }
+
+            files.forEach { res ->
+                val stream = javaClass.getResourceAsStream("/launcher/$res")
+                    ?: throw FileNotFoundException("resource `$res` not in classpath")
+
+                stream.use { `in` ->
+                    (launcherDir / res).outputStream().use { out ->
+                        `in`.copyTo(out)
+                    }
+                }
+            }
+        }
+
+        return launcherDir
+    }
+
+    private fun parseChecksums(): HashMap<Long, String> =
+        (javaClass.getResource("/launcher/checksums") ?: error("missing checksums"))
+            .readText()
+            .lineSequence()
+            .filter { it.isNotBlank() }
+            .map { it.trim().split(':', limit = 2) }
+            .associateTo(HashMap()) { it[0].toLong() to it[1] }
+    private fun File.checksum(): Long {
+        ADLER_32.reset()
+        ADLER_32.update(readBytes())
+        return ADLER_32.value
+    }
+    companion object {
+        val ADLER_32 by lazy { Adler32() }
     }
 }
