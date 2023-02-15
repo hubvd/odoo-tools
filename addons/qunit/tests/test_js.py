72294c3dce413fc3f24261db30d7bb939bbb1e7b
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-02-15 14:14:35 +0100

    Use --watch flag for every test

--- addons/qunit/tests/test_js.py
+++ addons/qunit/tests/test_js.py
@@ -1,13 +1,12 @@
 import os
 import odoo.tests
-import odoo.release
 
 
 def qunit_error_checker(message):
     # We don't want to stop qunit if a qunit is breaking.
 
     # '%s/%s test failed.' case: end message when all tests are finished
-    if 'tests failed.' in message:
+    if "tests failed." in message:
         return True
 
     # "QUnit test failed" case: one qunit failed. don't stop in this case
@@ -17,7 +16,7 @@ def qunit_error_checker(message):
     return True  # in other cases, always stop (missing dependency, ...)
 
 
-@odoo.tests.tagged('post_install', '-at_install')
+@odoo.tests.tagged("post_install", "-at_install")
 class WebSuite(odoo.tests.HttpCase):
     url = "/web/tests?mod=web"
 
@@ -29,18 +28,13 @@ class WebSuite(odoo.tests.HttpCase):
         filter = os.environ.get("QUNIT_FILTER", "!Numbering system")
         url += f"&filter={filter}"
 
-        version = odoo.release.version_info[0]
-        if isinstance(version, str):
-            version = int(version.lstrip("saas~"))
+        self.browser_js(
+            url, "", "", login="admin", timeout=1800, error_checker=qunit_error_checker
+        )
 
-        if version >= 16:
-            watch = os.environ.get("QUNIT_WATCH") == "1"
-            self.browser_js(url, "", "", login='admin', timeout=1800, error_checker=qunit_error_checker, watch=watch)
-        else:
-            self.browser_js(url, "", "", login='admin', timeout=1800)
 
-@odoo.tests.tagged('post_install', '-at_install')
+@odoo.tests.tagged("post_install", "-at_install")
 class WebSuiteMobile(WebSuite):
     url = "/web/tests/mobile?mod=web"
-    browser_size = '375x667'
+    browser_size = "375x667"
     touch_enabled = True
--- odoo/resources/launcher/patches/tests.py
+++ odoo/resources/launcher/patches/tests.py
@@ -1,22 +1,50 @@
-from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn
+from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    BarColumn,
+    TaskProgressColumn,
+    TimeRemainingColumn,
+)
 
 import odoo
 import unittest
 import time
 import logging
 from odoo.tests.runner import OdooTestResult
+from odoo.tests import HttpCase
 from odoo import sql_db
+import os
+
 
 _logger = logging.getLogger("odoo.tests.runner")
+
+
 def startTest(super):
     def decorator(self, test):
-        _logger.info(f"Starting [bold steel_blue1 underline]{self.getDescription(test)}[/]", extra={"markup": True})
+        _logger.info(
+            f"Starting [bold steel_blue1 underline]{self.getDescription(test)}[/]",
+            extra={"markup": True},
+        )
         super(self, test)
         self.time_start = time.time()
         self.queries_start = sql_db.sql_counter
+
     return decorator
 
-class ProgressTestResultPatch:
 
+def browser_js(super):
+    def decorator(self, *args, **kwargs):
+        if os.environ.get("QUNIT_WATCH") == "1":
+            kwargs["watch"] = True
+        super(self, *args, **kwargs)
+
+    return decorator
+
+
+class ProgressTestResultPatch:
     def apply(self):
-        odoo.tests.runner.OdooTestResult.startTest = startTest(unittest.result.TestResult.startTest)
+        odoo.tests.runner.OdooTestResult.startTest = startTest(
+            unittest.result.TestResult.startTest
+        )
+        odoo.tests.HttpCase.browser_js = browser_js(odoo.tests.HttpCase.browser_js)
