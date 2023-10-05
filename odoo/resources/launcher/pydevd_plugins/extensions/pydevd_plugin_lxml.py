df894b9d45987bef233d7f96d2a78a7f89800d54
Hubert Van de Walle (huvw) <huvw@odoo.com>
2023-10-05 10:34:18 +0200

    Fix pydevd plugin

--- odoo/resources/launcher/pydevd_plugins/extensions/pydevd_plugin_lxml.py
+++ odoo/resources/launcher/pydevd_plugins/extensions/pydevd_plugin_lxml.py
@@ -6,5 +6,5 @@ class LxmlProvider(StrPresentationProvider):
     def can_provide(self, type_object, type_name):
         return issubclass(type_object, etree._Element)
 
-    def get_str(self, val):
+    def get_str(self, val, *args):
         return etree.tostring(val, encoding="utf-8").decode("utf-8")
