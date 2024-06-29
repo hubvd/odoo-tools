efc54c9103bb889ea8b2cdb0293a935d78bfa5dd
Hubert Van De Walle <huvw@odoo.com>
2024-06-29 11:29:21 +0200

    login button -> change event

--- userscripts/odoo-login.js
+++ userscripts/odoo-login.js
@@ -1,29 +1,25 @@
 // ==UserScript==
 // @name        Login odoo admin/demo
+
 // @match       https://*.runbot*.odoo.com/web/login
 // @match       https://*.runbot*.odoo.com/*/web/login
+
 // @match       http://localhost:*/web/login
 // @match       http://localhost:*/*/web/login
+//
+// @match       http://*.localhost:*/web/login
+// @match       http://*.localhost:*/*/web/login
+
 // @match       http://127.*.*.*:*/web/login
 // @match       http://127.*.*.*:*/*/web/login
+
 // @grant       none
 // @version     1.0
 // @author      hubvd
 // ==/UserScript==
 
-window.addEventListener('load', () => {
-  const login = document.querySelector('.oe_login_form button[type="submit"]')
-  for (let text of ['admin', 'demo', 'portal']) {
-    const el = document.createElement('button')
-    el.innerText = text
-    el.classList.add('btn', 'btn-info', 'btn-block')
-    el.addEventListener('click', (e) => {
-      document.getElementById('login').value = text
-      document.getElementById('password').value = text
-      e.preventDefault()
-      document.querySelector('form.oe_login_form').submit()
-    })
-    login.parentNode.insertBefore(el, login)
-  }
-})
+login?.addEventListener("change", ({ target }) => {
+  const user = { a: "admin", d: "demo", p: "portal" }[target.value];
+  if (user) target.value = target.form.password.value = user;
+});
 
