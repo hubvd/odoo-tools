1d11787e5115b6ff730305beac49b98d5c7c7478
Hubert Van De Walle <huvw@odoo.com>
2025-05-14 22:36:40 +0200

    add author to debug module

--- addons/debug/__manifest__.py
+++ addons/debug/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "debug",
     "depends": ["base"],
+    "author": "huvw",
     "description": "Adds a route to attach to a pycharm debug server",
     "auto_install": True,
     "license": "Other OSI approved licence",
