9c05c0a9ec22cfece51539cfc513ee45551703ce
Hubert Van De Walle <huvw@odoo.com>
2025-05-26 14:30:28 +0200

    Updates

--- libgit/next/build.gradle.kts
+++ libgit/next/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 dependencies {
     compileOnly(libs.graalvm.svm)
     api(libs.serialization.json)
-    implementation("com.squareup:kotlinpoet:2.1.0")
+    implementation("com.squareup:kotlinpoet:2.2.0")
 
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
