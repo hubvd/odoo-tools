04c6e599b54052ccddb57a721ec66cfc332d1aec
Hubert Van De Walle <huvw@odoo.com>
2025-05-26 14:30:28 +0200

    change formatting wip

--- odoo/src/commands/Widgets.kt
+++ odoo/src/commands/Widgets.kt
@@ -1,8 +1,8 @@
 package com.github.hubvd.odootools.odoo.commands
 
-import com.github.ajalt.mordant.rendering.*
+import com.github.ajalt.mordant.rendering.TextAlign
+import com.github.ajalt.mordant.rendering.TextColors
 import com.github.ajalt.mordant.table.verticalLayout
-import com.github.ajalt.mordant.terminal.Terminal
 import com.github.ajalt.mordant.widgets.Panel
 import com.github.ajalt.mordant.widgets.Text
 import com.github.hubvd.odootools.odoo.RunConfiguration
@@ -10,7 +10,7 @@ import com.github.hubvd.odootools.odoo.RunConfiguration
 fun runConfigurationWidget(runConfiguration: RunConfiguration) = verticalLayout {
     cell(
         Panel(
-            EnvWidget(runConfiguration),
+            envText(runConfiguration),
             title = Text("Env"),
             titleAlign = TextAlign.LEFT,
             expand = true,
@@ -18,7 +18,7 @@ fun runConfigurationWidget(runConfiguration: RunConfiguration) = verticalLayout 
     )
     cell(
         Panel(
-            ArgWidget(runConfiguration),
+            argText(runConfiguration),
             title = Text("Arguments"),
             titleAlign = TextAlign.LEFT,
             expand = true,
@@ -26,71 +26,46 @@ fun runConfigurationWidget(runConfiguration: RunConfiguration) = verticalLayout 
     )
 }
 
-class EnvWidget(private val runConfiguration: RunConfiguration) : Widget {
-    override fun measure(t: Terminal, width: Int): WidthRange = WidthRange(
-        "workspace=${runConfiguration.odoo.workspace.path}".length,
-        "workspace=${runConfiguration.odoo.workspace.path}".length,
-    )
-
-    override fun render(t: Terminal, width: Int) = Lines(
-        buildList(2 + runConfiguration.env.size) {
-            add(
-                Line(
-                    listOf(
-                        Span.word("workspace", TextColors.green),
-                        Span.word("=", TextColors.gray),
-                        Span.word("${runConfiguration.odoo.workspace.path}"),
-                    ),
-                ),
-            )
-            add(
-                Line(
-                    listOf(
-                        Span.word("version", TextColors.green),
-                        Span.word("=", TextColors.gray),
-                        Span.word("${runConfiguration.odoo.workspace.version}"),
-                    ),
-                ),
-            )
-            runConfiguration.env.forEach { (k, v) ->
-                add(
-                    Line(
-                        listOf(
-                            Span.word(k, TextColors.green),
-                            Span.word("=", TextColors.gray),
-                            Span.word(v),
-                        ),
-                    ),
-                )
+fun envText(runConfiguration: RunConfiguration): Text = Text(
+    buildString {
+        val padding = runConfiguration.env.keys.maxOf { it.length } + 2
+        runConfiguration.env.forEach { (k, v) ->
+            append(TextColors.green(k))
+            repeat(padding - k.length) {
+                append(' ')
             }
-        },
-    )
-}
-
-class ArgWidget(private val runConfiguration: RunConfiguration) : Widget {
-    override fun measure(t: Terminal, width: Int): WidthRange {
-        val width = runConfiguration.args.maxOf { it.length }
-        return WidthRange(width, width)
-    }
-
-    override fun render(t: Terminal, width: Int) = Lines(
-        buildList(runConfiguration.args.size) {
-            runConfiguration.args.forEach { arg ->
-                val line = ArrayList<Span>(3)
-                val parts = arg.split('=', limit = 2)
-                line += Span.word(parts[0], style = TextColors.magenta)
-                if (parts.size > 1) {
-                    line += Span.word("=", style = TextColors.gray)
-                    val words = parts[1].split(" ")
-                    val first = words.dropLast(1)
-                    first.forEach {
-                        line += Span.word(it)
-                        line += Span.space()
-                    }
-                    line += Span.word(words.last())
-                }
-                add(Line(line))
+            append(TextColors.gray("="))
+            append(' ')
+            if (k == "PATH") {
+                append(v.substringBefore(':'))
+                append(":...")
+            } else {
+                append(v)
             }
-        },
-    )
-}
+            append('\n')
+        }
+        setLength(length - 1)
+    },
+)
+
+fun argText(runConfiguration: RunConfiguration): Text = Text(
+    buildString {
+        val maxEqIndex = runConfiguration.args
+            .maxOfOrNull { it.indexOf('=').takeIf { it != -1 } ?: it.length }
+            ?: 0
+
+        runConfiguration.args.forEach { arg ->
+            val parts = arg.split('=', limit = 2)
+            repeat(maxEqIndex - parts[0].length) {
+                append(' ')
+            }
+            append(TextColors.magenta(parts[0]))
+            if (parts.size > 1) {
+                append(TextColors.gray("="))
+                append(parts[1])
+            }
+            append('\n')
+        }
+        setLength(length - 1)
+    },
+)
